341e22f1c0b3c5eda79e2b5ef094439adb6efe36
insoftver <d.insoftver@gmail.com>
2020-12-23 14:45:49 -0600

    Updated ruby/methods/transcode.rb

--- ruby/methods/transcode.rb
+++ ruby/methods/transcode.rb
@@ -1,4 +1,20 @@
+#!/bin/ruby
+
+# This script encodes a given string into UTF-8 if
+# its own encoding is different.
+
+# The function accepts: 
+  # =>  string(String), string to be encoded.
+
 def transcode(string)
-	return string.force_encoding(Encoding::UTF_8)
+  return string.force_encoding(Encoding::UTF_8) if string.encoding != 'UTF-8'
 end
-puts transcode('Diego').encoding
+
+# Usage
+
+# puts String(123456).encoding
+# => US-ASCII
+# puts transcode(String(123456)).encoding
+# => UTF-8
+
+#
