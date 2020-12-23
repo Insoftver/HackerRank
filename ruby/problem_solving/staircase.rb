ea5643afa4a114c4626d327e3dd10cda61ab094e
insoftver <d.insoftver@gmail.com>
2020-12-23 15:16:07 -0600

    Added ruby/problem_solving/staircase.rb

---
+++ ruby/problem_solving/staircase.rb
@@ -0,0 +1,29 @@
+#!/bin/ruby
+
+# This script solves the next problem.
+# https://www.hackerrank.com/challenges/staircase/problem
+# The function is expected to print a staircase based on 
+# a given size value as shown below.
+
+# size = 5
+# =>    #
+       ##
+      ###
+     ####
+    #####
+
+# The function accepts: 
+  # => size(Integer), size of the staircase.
+
+def staircase(size)
+  size.times do |counter|
+    puts "#{" " * ((size-counter)-1)}" + "#{"#" * (counter+1)}"
+  end
+end
+
+# => size = 5 # <= testVar
+
+size = gets.to_i until (1..100).include?(size)
+staircase(size)
+
+#
