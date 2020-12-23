ea05d21ebf12f64c30e02ae126702b47555ad453
insoftver <d.insoftver@gmail.com>
2020-12-23 08:36:44 -0600

    Modified problem_solving/easy/diagonalDifference.rb

--- problem_solving/easy/diagonalDifference.rb
+++ problem_solving/easy/diagonalDifference.rb
@@ -2,7 +2,6 @@
 
 # => This script is related to the next problem.
 # https://www.hackerrank.com/challenges/diagonal-difference/problem
-# Complete the 'diagonalDifference' function below.
 # The function is expected to return an INTEGER.
 # The function accepts 2D_INTEGER_ARRAY arr as parameter.
 
