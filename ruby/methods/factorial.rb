a6b0c30617ebae168a89812e0507de519b43c8a4
insoftver <d.insoftver@gmail.com>
2020-12-23 13:07:59 -0600

    Updated ruby/methods/factorial.rb

--- ruby/methods/factorial.rb
+++ ruby/methods/factorial.rb
@@ -1,13 +1,39 @@
-def factorial(n)
-   yield n
+#!/bin/ruby
+
+# This script prints the factorial of a number.
+# The function is expected to return an Integer.
+
+# The function accepts: 
+  # =>  number(Integer), number to factorize.
+
+def factorial(number)
+   yield number
 end
 
-n = gets.to_i
+number = *ARGV # => Array
+number = number.join('|').to_i
 
-factorial(n) do |number| 
+factorial(number) do |num| 
   factor = 1
-  number.times do |counter|
-	 	factor += (factor*counter)
+  num.times do |counter|
+    factor += (factor*counter)
   end
   puts factor
 end
+
+# Usage from command line...
+
+# ruby factorial.rb 5
+# => 120
+# ruby factorial.rb 6
+# => 720
+# ruby factorial.rb 7
+# => 5040
+# ruby factorial.rb 8
+# => 40320
+# ruby factorial.rb 9
+# => 362880
+# ruby factorial.rb 10
+# => 3628800
+
+#
