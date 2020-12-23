15539fba26ba1ae06205a228dcdbf109c839ea70
insoftver <d.insoftver@gmail.com>
2020-12-23 08:25:02 -0600

    Modified problem_solving/easy/aVerBigSum.rb

--- problem_solving/easy/aVeryBigSum.rb
+++ problem_solving/easy/aVeryBigSum.rb
@@ -1,15 +1,18 @@
 #!/bin/ruby
 
-# This code is related to this problem
+# This scripts solves this problem.
 # https://www.hackerrank.com/challenges/a-very-big-sum/problem
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY as parameter.
 
-# Complete the aVeryBigSum function below.
-def aVeryBigSum(ar)
-  return ar.inject {|sum,num| sum+num}
+def aVeryBigSum(array)
+  return array.inject {|sigma,number| sigma+number}
 end
 
-ar_count = gets.to_i until (1..10).include?(ar_count)
-ar = gets.rstrip.split(' ').map(&:to_i)
-ar.size {i| ar[i] = gets.to_i until (-2147483648..2147483648).include?(ar[i])}
-result = aVeryBigSum ar
+arrayCount = gets.to_i until (1..10).include?(arrayCount)
+array = gets.rstrip.split(' ').map(&:to_i)
+array.size {|i| array[i] = gets.to_i until (-2**31..2**31).include?(array[i])}
+result = aVeryBigSum array
 puts result
+
+#
