d7b25d9f45d90b445f41046a4413662b91f4ae50
insoftver <d.insoftver@gmail.com>
2020-12-23 09:16:38 -0600

    Modified problem_solving/easy/aVeryBigSum.rb

--- problem_solving/easy/aVeryBigSum.rb
+++ problem_solving/easy/aVeryBigSum.rb
@@ -3,7 +3,7 @@
 # This script solves the next problem.
 # https://www.hackerrank.com/challenges/a-very-big-sum/problem
 # The function is expected to return an INTEGER.
-# The function accepts INTEGER_ARRAY as parameter.
+# The function accepts an INTEGER_ARRAY as parameter.
 
 def aVeryBigSum(array)
   return array.inject {|sigma,number| sigma+number}
