84692556dd6098ae66bc4d424bb84ae200b481e6
insoftver <d.insoftver@gmail.com>
2020-12-23 12:36:50 -0600

    Updated ruby/methods/count_multibyte_char.rb

--- ruby/methods/count_multibyte_char.rb
+++ ruby/methods/count_multibyte_char.rb
@@ -1,12 +1,25 @@
+#!/bin/ruby
+
+# This script counts multibyte characters on a given string.
+# The function is expected to return an Integer.
+
+# The function accepts: 
+  # =>  string(String), string to count.
+
 def count_multibyte_char(string)
-	bytes = Array.new(string.size,0)
-	countChar = 0
-	string.each_char do |char|
-		char.each_byte {|byte|bytes[countChar] += 1}
-		countChar += 1
-	end
-	charsBiggerThanOneByte = 0
-	bytes.each {|byte|charsBiggerThanOneByte += 1 if byte > 1}
-	return charsBiggerThanOneByte
+  bytes = Array.new(string.size,0)
+  countChars = 0
+  string.each_char do |char|
+    char.each_byte {|byte|bytes[countChars] += 1}
+    countChars += 1
+  end
+  charsBiggerThanOneByte = 0
+  bytes.each {|byte|charsBiggerThanOneByte += 1 if byte > 1}
+  return charsBiggerThanOneByte
 end
-puts count_multibyte_char('¥hola') # => 1
+
+# puts count_multibyte_char('¥hola') 
+# => 1
+
+# puts count_multibyte_char('hola') 
+# => 0
