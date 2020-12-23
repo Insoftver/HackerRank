bc2ffe1234bc648d75241089ad33161bde81f40c
insoftver <d.insoftver@gmail.com>
2020-12-23 14:26:52 -0600

    Updated ruby/methods/process_text.rb

--- ruby/methods/process_text.rb
+++ ruby/methods/process_text.rb
@@ -1,6 +1,22 @@
+#!/bin/ruby
+
+# This script erases empty spaces at the beggining
+# and the end of string and removed its last element.
+# These string elements are set ont an array.
+
+# The function accepts: 
+  # =>  array(Array), array to be cleaned.
+
+
 def process_text(array)
-	array.size.times {|i| array[i] = array[i].strip.chomp}
-	return array.join(' ')
+  array.size.times {|i| array[i] = array[i].strip.chomp}
+  return array.join(' ')
 end
-process_text(["Hi, \n", " Are you having fun?    "])
+
+
+# Usage
+
+# process_text(["Hi, \n", " Are you having fun?    "])
 # => "Hi, Are you having fun?"
+
+#
