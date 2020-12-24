a232b1febc3b95cbcad16318b7909a86d3a4c499
insoftver <d.insoftver@gmail.com>
2020-12-24 15:34:03 -0600

    Modified currying.rb

--- ruby/notes/currying.rb
+++ ruby/notes/currying.rb
@@ -15,9 +15,9 @@
 
 
 ##############################################################
-### Example
+### Example 1
 ##############################################################
-  ### EXAMPLE01
+  ### EXAMPLE 1A
   # Method
   def example(a,b,c)
     [a, b, c]
@@ -27,7 +27,7 @@
     proc02 = proc.call(1, 2) #=> #<Proc>
     proc02.call(3) #=> [1,2,3]
 
-  ### EXAMPLE02
+  ### EXAMPLE 1B
   # Method
     def example02(*args)
       args
@@ -37,4 +37,28 @@
     proc2 = proc.call(:x) #=> #<Proc>
     proc3 = proc2.call(:y, :z) #=> #<Proc>
     proc3.call(:a) #=> [:x, :y, :z, :a]
+##############################################################
+
+##############################################################
+### Example 2
+##############################################################  
+  # In this example the lambda multiply_numbers take two 
+  # parameters, and return the product of the two;
+    multiply_numbers = -> (x,y) do
+      x*y
+    end
+    # or
+    multiply_numbers = -> (x,y) {x*y}
+
+  # multiply_numbers.curry.(2) returns a lambda(doubler) 
+  # which takes only one parameter necessary for calculation
+  # and process it according with its starting calculation
+    doubler = multiply_numbers.curry.(2) # <- new lambda
+  # Finally we use the lambda doubler in another process.
+    puts doubler.(4)    #8
+
+  # => FINALLY
+  # Another instance of the same lambda now with 3.
+    tripler = multiply_numbers.curry.(3)# <- new lambda
+    puts tripler.(4)    #12
 ##############################################################
