270011a50e3b22fc245e7801616836cc9a8bad23
insoftver <d.insoftver@gmail.com>
2020-12-24 07:30:23 -0600

    Modified birthdayCakeCandles.rb

--- ruby/problem_solving/birthdayCakeCandles.rb
+++ ruby/problem_solving/birthdayCakeCandles.rb
@@ -11,8 +11,6 @@ end
 
 candlesCount = 0
 candles = Array.new(candlesCount,0)
-# candlesCount = 4 # => testVar
-# candles = '3 2 1 3'.split(' ').map(&:to_i) # => testVar
 candlesCount = gets.to_i until (1..10**5).include?(candlesCount)
 candles = gets.strip.split(' ').map(&:to_i)
 candles.size.times {|i| candles[i] = gets.strip.to_i until (1..10**7).include?(candles[i])}
