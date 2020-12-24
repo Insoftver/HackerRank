86a85b4e275c691f847ad14c16c060d66f42e743
insoftver <d.insoftver@gmail.com>
2020-12-24 15:48:14 -0600

    Modified powerFunction.rb.

--- ruby/problem_solving/powerFunction.rb
+++ ruby/problem_solving/powerFunction.rb
@@ -4,16 +4,16 @@
 # https://www.hackerrank.com/challenges/ruby-curry/problem
 # It is aimed to show how the curry method works with lambdas.
 # The lambda powerFunction pows an INTEGER.
-# The raise_to_power curried lambda takes the base.
+# The raiseToPower curried lambda takes the base.
 # Finally we used the curried lambda with just the 
 # power value as parameter
 
 powerFunction = -> (x, z) {(x) ** z}
 
 base = gets.to_i
-raise_to_power = powerFunction.curry.(base)
+raiseToPower = powerFunction.curry.(base)
 
 power = gets.to_i
-puts raise_to_power.(power)
+puts raiseToPower.(power)
 
 #
