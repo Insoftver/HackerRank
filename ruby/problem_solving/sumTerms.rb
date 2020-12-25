48a0c1e4e1be91e5cd8ead3aac9c467ecfe865dd
insoftver <d.insoftver@gmail.com>
2020-12-24 23:06:55 -0600

    Modified sumTerms.rb

--- ruby/problem_solving/sumTerms.rb
+++ ruby/problem_solving/sumTerms.rb
@@ -8,10 +8,10 @@
 # https://ruby-doc.org/core-2.5.1/Enumerable.html
 
 def sumTerms number
-	(0..n).inject {|sigma,counter| sigma + (counter * counter + 1 )}
+	(0..number).inject {|sigma,counter| sigma + (counter * counter + 1 )}
 end
-puts sumTerms 5
 
-# => 
+# puts sumTerms 5
+# => 60
 
 #
