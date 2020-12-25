58a4e3b9cdace800db8462a04493426c713ec1f5
insoftver <d.insoftver@gmail.com>
2020-12-25 00:04:34 -0600

    Modified notes/enumerable.rb.

--- ruby/notes/enumerable.rb
+++ ruby/notes/enumerable.rb
@@ -78,8 +78,56 @@
   [1, 2i, 3.14].all?(Numeric) #=> true
   [nil, true, 99].all? #=> false
   [].all? #=> true
+  
+# example from problem_solving/enumerables.rb
+  def func_all(hash)
+      # Check and return true if all the values within the hash are Integers
+      # and are < 10. If not all values satisfy this, return false.
+      hash.all? {|key,value|value.is_a? Integer && value < 10}
+  end
 #############################################################################
 
+
+#############################################################################
+### any
+#############################################################################
+  # example from problem_solving/enumerables.rb
+  def func_any(hash)
+    # Check and return true if any key object within the hash is of the type Integer
+    # If not found, return false.
+    hash.any? {|key,value|key.is_a? Integer}
+  end
+#############################################################################
+
+
+#############################################################################
+### none
+#############################################################################
+  # example from problem_solving/enumerables.rb
+  def func_none(hash)
+    # Check and return true if none of the values within the hash are nil
+    # If any value contains nil, return false.
+    hash.none? {|value| element.nil?}
+  end
+#############################################################################
+
+
+#############################################################################
+### find
+#############################################################################
+def func_find(hash)
+    # Check and return the first object that satisfies either of the following 
+    # properties:
+      # 1. There is a [key, value] pair where the key and value are both 
+      # Integers and the value is < 20 
+      # 2. There is a [key, value] pair where the key and value are both 
+      # Strings and the value starts with `a`.
+    hash.find do |key, value|
+      (key.is_a?(Integer) && value.is_a?(Integer) && v < 20) || 
+      (key.is_a?(String) && value.is_a?(String) && v.start_with?('a'))
+    end
+end
+#############################################################################
 
 #############################################################################
 ### each
