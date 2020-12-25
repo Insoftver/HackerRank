92859241e4ec41edae2089cfc5e876c35f54e3d6
insoftver <d.insoftver@gmail.com>
2020-12-24 18:20:56 -0600

    Added iterate_colors.rb.

---
+++ ruby/methods/iterate_colors.rb
@@ -0,0 +1,18 @@
+#!/bin/ruby
+
+# This script solves this problem.
+# https://www.hackerrank.com/challenges/ruby-enumerable-introduction/problem
+# To solve it this source was studied.
+# https://ruby-doc.org/core-2.7.2/Enumerable.html
+
+def iterate_colors(colors)
+  counter = 0
+  values = Array.new
+  colors.each do |color|
+      values[counter] = color
+      counter += 1
+  end
+  return values
+end
+
+#
