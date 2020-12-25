e07cd64924ef949ed6f533262c8240192d31f5c8
insoftver <d.insoftver@gmail.com>
2020-12-24 18:45:26 -0600

    Modified enumerable.rb.

--- ruby/notes/enumerable.rb
+++ ruby/notes/enumerable.rb
@@ -1,9 +1,33 @@
-#!/bin/ruby
+#############################################################################
+### Enumerable
+### https://ruby-doc.org/core-2.7.2/Enumerable.html
+#############################################################################
+  # The Enumerable mixin provides collection classes with several traversal 
+  # and searching methods, and with the ability to sort. The class must 
+  # provide a method each, which yields successive members of the collection. 
+  # If #max, min, or sort is used, the objects in the collection must also 
+  # implement a meaningful <=> operator, as these methods rely on an ordering 
+  # between members of the collection.
+
+  # NOTE: English word curry may be understood as well as 
+  # 'prepare', 'dress'(as in restaurant), 'smooth'.
+#############################################################################
+
+
+#############################################################################
+#############################################################################
+  %w[ant bear cat].all? { |word| word.length >= 3 } #=> true
+  %w[ant bear cat].all? { |word| word.length >= 4 } #=> false
+  %w[ant bear cat].all?(/t/) #=> false
+  [1, 2i, 3.14].all?(Numeric) #=> true
+  [nil, true, 99].all? #=> false
+  [].all? #=> true
+#############################################################################  
+
 
 # This script solves this problem.
 # https://www.hackerrank.com/challenges/ruby-enumerable-each-with-index/problem
 # To solve it this source was studied.
-# https://ruby-doc.org/core-2.7.2/Enumerable.html
 
 def iterate_colors(colors)
   counter = 0
