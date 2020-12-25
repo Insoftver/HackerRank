8f81bf2f03bb6c491a8202eff2e9141b1f33b2bb
insoftver <d.insoftver@gmail.com>
2020-12-24 23:04:35 -0600

    Modified sumTerms.rb

--- ruby/problem_solving/sumTerms.rb
+++ ruby/problem_solving/sumTerms.rb
@@ -5,7 +5,7 @@
 
 # To solve it this source was studied.
 # (Portuguese) https://www.infoescola.com/matematica/sequencias-numericas/
-# 
+# https://ruby-doc.org/core-2.5.1/Enumerable.html
 
 def sumTerms number
 	(0..n).inject {|sigma,counter| sigma + (counter * counter + 1 )}
