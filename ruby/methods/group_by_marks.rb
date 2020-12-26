6df1909073c615118b126c42fd7a1ed4671a2bea
insoftver <d.insoftver@gmail.com>
2020-12-26 14:48:46 -0600

    Modified group_by_mark.rb.

--- ruby/methods/group_by_marks.rb
+++ ruby/methods/group_by_marks.rb
@@ -11,5 +11,6 @@ end
 
 # marks = {"Ramesh":23, "Vivek":40, "Harsh":88, "Mohammad":60}
 # puts group_by_marks marks, 30
+# => {"Failed"=>[[:Ramesh, 23]], "Passed"=>[[:Vivek, 40], [:Harsh, 88], [:Mohammad, 60]]}
 
 #
