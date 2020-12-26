747389f4b4ea508f47f19dee696ed512ad09cd06
insoftver <d.insoftver@gmail.com>
2020-12-26 15:52:10 -0600

    Modified group_by_mark.rb.

--- ruby/methods/group_by_marks.rb
+++ ruby/methods/group_by_marks.rb
@@ -6,7 +6,7 @@
 # The function accepts a Hash and an Integer as parameters.
 
 def group_by_marks(marks,pass_marks)
-	marks.group_by {|key,value| value < pass_marks ? "Failed" : "Passed" }
+  marks.group_by {|key,value| value < pass_marks ? "Failed" : "Passed" }
 end
 
 # marks = {"Ramesh":23, "Vivek":40, "Harsh":88, "Mohammad":60}
