3f42bf4d65bfde01a9df8ee52cd2350ccdeddfc0
Diego Mota <d.insoftver@gmai.com>
2020-12-28 14:10:51 -0600

    Modified catAndMouse.rb

--- ruby/problem_solving/catAndMouse.rb
+++ ruby/problem_solving/catAndMouse.rb
@@ -9,31 +9,31 @@
   # Two Integer arrays wih the locations.
 
 
-def catAndMouse(x, y, z)	catA = distance(x,z)
-	catB = distance(y,z)
-	if catA == catB
-		return 'Mouse C' 
-	else
-		if catA > catB then return 'Cat B' else return 'Cat A' end
-	end
+def catAndMouse(x, y, z)  catA = distance(x,z)
+  catB = distance(y,z)
+  if catA == catB
+    return 'Mouse C' 
+  else
+    if catA > catB then return 'Cat B' else return 'Cat A' end
+  end
 end
 
 def distance(c,m) # => cat/mouse
-	return c >= m ? (c-m).abs : (m-c).abs#olute
+  return c >= m ? (c-m).abs : (m-c).abs#olute
 end
 
 q=1 # => test var
 #q = gets.to_i until (1..100).include?(q)
 
 q.times do |q_itr|
-	xyz = '1 3 2'.split(' ').map(&:to_i) # => test var
-	#xyz = gets.rstrip.split(' ').map(&:to_i)
-	xyz.size {|i| xyz[i] = gets until (1..100).include(xyz[i])}
-	x = xyz[0]
-	y = xyz[1]
-	z = xyz[2]
-	result = catAndMouse x, y, z
-	puts result
+  xyz = '1 3 2'.split(' ').map(&:to_i) # => test var
+  #xyz = gets.rstrip.split(' ').map(&:to_i)
+  xyz.size {|i| xyz[i] = gets until (1..100).include(xyz[i])}
+  x = xyz[0]
+  y = xyz[1]
+  z = xyz[2]
+  result = catAndMouse x, y, z
+  puts result
 end
 
 #
