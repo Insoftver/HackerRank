aeb2f7cb4922906d015571abe22948dcd2ccdee6
insoftver <d.insoftver@gmail.com>
2020-12-29 15:39:25 -0600

    Added miniMaxSum.rb.

--- ruby/problem_solving/miniMaxSum.rb
+++ ruby/problem_solving/miniMaxSum.rb
@@ -2,24 +2,27 @@
 
 # This script solves the next problem.
 # https://www.hackerrank.com/challenges/mini-max-sum/problem
-# The function is expected to return 2 space separated INTEGERS.
-# The function accepts an INTEGER_ARRAY as parameter.
+# The method is expected to return 2 Integers.
+# The method accepts an Array as parameter.
 
 def miniMaxSum(array)
-  results = Array.new(array.size,0)
-  skip = 0
-  array.size.times do |index|
-    array.size.times do |skipIndex|
-      skip == skipIndex ? results[index] += 0 : results[index] += array[skipIndex]
+  results = Array.new(array.size) { |i| i = 0 }
+  omit = 0
+  array.size.times do |i|
+    array.size.times do |j|
+      if omit == j 
+        results[i] += 0
+      else
+        results[i] += array[j]
+      end
     end
-    skip += 1
+    omit += 1
   end
   return results.min, results.max
 end
 
-array = Array.new
-array = gets.rstrip.split(' ').map(&:to_i)
-array.size.times {|i| array[i] = gets.to_i until (1..10**9).include?(array[i])}
+array = gets.strip.split(' ').map(&:to_i)
+array.each {|a|a = gets.to_i until (1..10**9).include?(a)}
 minimum, maximum = miniMaxSum array
 puts "#{minimum} #{maximum}"
 
