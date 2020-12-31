890ac826ae24cb23788c184c4c48b8c7c6a37405
insoftver <d.insoftver@gmail.com>
2020-12-31 06:28:08 -0600

    Modified factorial.js.

--- javascript/tenDaysOfJavaScript/scripts/factorial.js
+++ javascript/tenDaysOfJavaScript/scripts/factorial.js
@@ -1,3 +1,40 @@
-function factorial (number) {
-  (number!=1) ? number * factorial(number-1) : 1;
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
 }
+/*
+ * This code is related to this problem.
+ * https://www.hackerrank.com/challenges/js10-function/problem
+ */
+function factorial (number) {
+  return (number!=1) ? number * factorial(number-1) : 1;
+  
+}
+
+
+function main() {
+    const n = +(readLine());
+    
+    console.log(factorial(n));
+}
+
+//
