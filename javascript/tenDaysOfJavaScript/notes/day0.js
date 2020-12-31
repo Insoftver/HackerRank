62dfee77913141956c2a1abc457c9e3d4c9e36d7
insoftver <d.insoftver@gmail.com>
2020-12-30 18:58:48 -0600

    Added day0.js.

---
+++ javascript/tenDaysOfJavaScript/notes/day0.js
@@ -0,0 +1,23 @@
+// These notes belongs to the day 0 of the 
+// 10 Days of JavaScript curse referenced above
+// https://www.hackerrank.com/domains/tutorials/10-days-of-javascript
+
+// The console.log() method writes a message to the console. 
+// The console is useful for testing purposes. It is used as
+// shown above.
+console.log('Text to print.');
+
+
+// parseInt converts a number into Integer.
+const integer = parseInt(string);
+    
+// parseFloat converts a number into Float.
+const floatSecondDecimal = parseFloat(secondDecimal);
+
+      
+// Concatenate strings
+const firstString = 'firstString'
+const secondString = 'secondString'
+const concatenation = firstString + secondString;
+
+#
