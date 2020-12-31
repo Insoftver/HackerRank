6469884921116bde9745cd06bf797cc241435fb0
insoftver <d.insoftver@gmail.com>
2020-12-31 06:26:38 -0600

    Added factorial.js.

---
+++ javascript/tenDaysOfJavaScript/notes/day1.js
@@ -0,0 +1,6 @@
+/* let 
+  This reserved word is used to delacer a variable.
+*/
+
+let number;
+number = 0;
---
+++ javascript/tenDaysOfJavaScript/scripts/factorial.js
@@ -0,0 +1,3 @@
+function factorial (number) {
+  (number!=1) ? number * factorial(number-1) : 1;
+}
