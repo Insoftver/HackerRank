31fbc9873095fb6583eef4551a6ccf16857aeb62
insoftver <d.insoftver@gmail.com>
2020-12-30 18:47:30 -0600

    Modified performOperation.js.

--- javascript/tenDaysOfJavaScript/scripts/performOperation.js
+++ javascript/tenDaysOfJavaScript/scripts/performOperation.js
@@ -7,19 +7,18 @@ let inputString = '';
 let currentLine = 0;
 
 process.stdin.on('data', inputStdin => {
-    inputString += inputStdin;
+  inputString += inputStdin;
 });
 
 process.stdin.on('end', _ => {
-    inputString = inputString.trim().split('\n').map(string => {
-        return string.trim();
-    });
-    
-    main();    
+  inputString = inputString.trim().split('\n').map(string => {
+    return string.trim();
+  });
+  main();    
 });
 
 function readLine() {
-    return inputString[currentLine++];
+  return inputString[currentLine++];
 }
 
 /**
@@ -32,33 +31,33 @@ function readLine() {
 *   2. The sum of 'firstDecimal' and the Number representation of 'secondDecimal'.
 *   3. The concatenation of 'firstString' and 'secondString' ('firstString' must be first).
 *
-*	Parameter(s):
+*  Parameter(s):
 *   secondInteger - The string representation of an integer.
 *   secondDecimal - The string representation of a floating-point number.
 *   secondString - A string consisting of one or more space-separated words.
 **/
 function performOperation(secondInteger, secondDecimal, secondString) {
-    // Declare a variable named 'firstInteger' and initialize with integer value 4.
-    const firstInteger = 4;
+  // Declare a variable named 'firstInteger' and initialize with integer value 4.
+  const firstInteger = 4;
     
-    // Declare a variable named 'firstDecimal' and initialize with floating-point value 4.0.
-    const firstDecimal = 4.0;
+  // Declare a variable named 'firstDecimal' and initialize with floating-point value 4.0.
+  const firstDecimal = 4.0;
     
-    // Declare a variable named 'firstString' and initialize with the string "HackerRank".
-    const firstString = 'HackerRank ';
+  // Declare a variable named 'firstString' and initialize with the string "HackerRank".
+  const firstString = 'HackerRank ';
     
-    // Write code that uses console.log to print the sum of the 'firstInteger' and 'secondInteger' (converted to a Number        type) on a new line.
-    const intSecondInteger = parseInt(secondInteger);
-    console.log(firstInteger + intSecondInteger);
+  // Write code that uses console.log to print the sum of the 'firstInteger' and 'secondInteger' (converted to a Number        type) on a new line.
+  const intSecondInteger = parseInt(secondInteger);
+  console.log(firstInteger + intSecondInteger);
     
-    // Write code that uses console.log to print the sum of 'firstDecimal' and 'secondDecimal' (converted to a Number            type) on a new line.
-    const floatSecondDecimal = parseFloat(secondDecimal);
-    const sum = firstDecimal + intSecondDecimal;
-    console.log(sum);
+  // Write code that uses console.log to print the sum of 'firstDecimal' and 'secondDecimal' (converted to a Number            type) on a new line.
+  const floatSecondDecimal = parseFloat(secondDecimal);
+  const sum = firstDecimal + intSecondDecimal;
+  console.log(sum);
     
-    // Write code that uses console.log to print the concatenation of 'firstString' and 'secondString' on a new line. The        variable 'firstString' must be printed first.
-    const concatenation = firstString + secondString;
-    console.log(concatenation);
+  // Write code that uses console.log to print the concatenation of 'firstString' and 'secondString' on a new line. The        variable 'firstString' must be printed first.
+  const concatenation = firstString + secondString;
+  console.log(concatenation);
 }
 
 #
