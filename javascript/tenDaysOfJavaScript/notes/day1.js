4c04449fc354890a4f219829d826d44ed4d54c25
insoftver <d.insoftver@gmail.com>
2020-12-31 06:44:29 -0600

    Added day1.js.

--- javascript/tenDaysOfJavaScript/notes/day1.js
+++ javascript/tenDaysOfJavaScript/notes/day1.js
@@ -1,6 +1,18 @@
 /* let 
   This reserved word is used to delacer a variable.
 */
-
 let number;
 number = 0;
+
+
+// This is how you create a function in JavbaScript
+function factorial (number) {
+  // This is a short if as Ruby
+  return (number!=1) ? number * factorial(number-1) : 1;
+}
+// And you call the function this way.
+factorial(5);
+
+
+// It is possible to add Math functions this way.
+const PI = Math.PI;
