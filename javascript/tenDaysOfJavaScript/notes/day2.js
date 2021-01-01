2a37c19f6aa34754b41ca751f02328ca1c1cd41e
insoftver <d.insoftver@gmail.com>
2021-01-01 09:23:01 -0600

    Modified day2.js.

--- javascript/tenDaysOfJavaScript/notes/day2.js
+++ javascript/tenDaysOfJavaScript/notes/day2.js
@@ -64,13 +64,35 @@ switch(letter){
 }
 
 // substring()
-// https://www.w3schools.com/jsref/jsref_substring.asp
+// The substring() method returns the part of the string 
+// between the start and end indexes, or to the end of the string.
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/substring
 let string ;
 string = 'Hello'
 character = string.substring(1, 4); // ell
 
 // atChar()
+// The String object's charAt() method returns a new string 
+// consisting of the single UTF-16 code unit located at the 
+// specified offset into the string.
 // https://www.w3schools.com/jsref/jsref_charat.asp
 character = string.atChar(1); // e
+
+// includes
+// This method works exatly as it does on Ruby.
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/includes
+const vowels = 'aeiou';
+for(var i = 0; i < s.length; i++) {
+  if (vowels.includes(s[i])) {
+    // something
+  } else {
+    consonants += s[i] + '\n';
+  }
+}
+
+// trim
+// The trim() method removes whitespace from both ends of a string.
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/trim
+console.log(consonants.trim());
 
 //
