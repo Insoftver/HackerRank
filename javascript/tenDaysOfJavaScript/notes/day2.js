d43bc83016f443c8a673c06bacbf4572ed435d2a
insoftver <d.insoftver@gmail.com>
2021-01-01 08:14:09 -0600

    Added day2.js.

---
+++ javascript/tenDaysOfJavaScript/notes/day2.js
@@ -0,0 +1,25 @@
+// Conditional if in JavaScript can be used this way.
+
+if (number == 30) {
+    console.log(30);
+}
+
+if (answer == 'Y') {
+  console.log('Yes');
+} else {
+  console.log('No');
+}
+
+if (score > 25) {
+  grade = 'A';
+} else if (score > 20) {
+  grade = 'B';
+} else if (score > 15) {
+  grade = 'C';
+} else if (score > 10) {
+  grade = 'D';
+} else if (score > 5) {
+  grade = 'E';
+} else if (score > 0) {
+  grade = 'F';
+}
