8bff0137f56ce5a69fc9ee1dc853bd2b301019a7
insoftver <d.insoftver@gmail.com>
2021-01-01 08:39:44 -0600

    Modified day2.js.

--- javascript/tenDaysOfJavaScript/notes/day2.js
+++ javascript/tenDaysOfJavaScript/notes/day2.js
@@ -1,4 +1,4 @@
-// Conditional if in JavaScript can be used this way.
+// Conditional if can be used this way.
 
 if (number == 30) {
     console.log(30);
@@ -23,3 +23,45 @@ if (score > 25) {
 } else if (score > 0) {
   grade = 'F';
 }
+
+// Conditional switch can be used this way
+
+  switch(letter){
+    case 'a':
+    case 'e':
+    case 'i':
+    case 'o':
+    case 'u':
+      letter = 'A';
+      break;
+    case 'b':
+    case 'c':
+    case 'd':
+    case 'f':
+    case 'g':
+      letter = 'B';
+      break;
+    case 'h':
+    case 'j':
+    case 'k':
+    case 'l':
+    case 'm':
+      letter = 'C';
+      break;
+    case 'n':
+    case 'p':
+    case 'q':
+    case 'r':
+    case 's':
+    case 't':
+    case 'v':
+    case 'w':
+    case 'x':
+    case 'y':
+    case 'z':
+      letter = 'D';
+      break;
+  }
+
+  // This method gets the first character of a string.
+  character = string.substring(0, 1);
